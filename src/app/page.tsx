7a86839f1a72e65c192f60649125be6297933f3d
Abhinav Sorate <abhinav.sorate@gida.io>
2023-11-21 00:35:07 +0530

    setup font and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import styles from "../styles/main.module.scss";
+import styles from "@/styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.test}>Initial Setup</h1>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.avatar}></div>
+      <div className={styles.headerTextBox}>
+        <h1 className={styles.headingPrimary}>Abhinav Sorate</h1>
+        <h2 className={styles.headingSecondary}>Software developer</h2>
+        <p className={styles.headingTertiary}>
+          Hi, I&apos;m a web and app developer based in Bengaluru, India. I
+          create modern websites, web apps and mobile apps that are fast,
+          responsive & easy to use.
+        </p>
+      </div>
+    </header>
+  );
 }
