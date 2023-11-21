4284e04640aaaee421fe51796a3fe8052838d3d1
Abhinav Sorate <abhinav.sorate@gida.io>
2023-11-21 08:50:06 +0530

    add typing animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       <div className={styles.avatar}></div>
       <div className={styles.headerTextBox}>
         <h1 className={styles.headingPrimary}>Abhinav Sorate</h1>
-        <h2 className={styles.headingSecondary}>Software developer</h2>
+        <h2 className={styles.headingSecondary}>
+          <span></span>
+        </h2>
         <p className={styles.headingTertiary}>
           Hi, I&apos;m a web and app developer based in Bengaluru, India. I
           create modern websites, web apps and mobile apps that are fast,
