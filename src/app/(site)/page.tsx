efc7648576030c553aab8bd4d61a8781c7784b66
Abhinav Sorate <abhinav.sorate@gida.io>
2023-11-23 17:54:44 +0530

    remove /about route

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className={styles.headerTextBox}>
           <h1 className={styles.headingPrimary}>Abhinav Sorate</h1>
           <h2 className={styles.headingSecondary}>
-            <span></span>
+            <span></span> {/* for typing animation */}
           </h2>
           <p className={styles.headingTertiary}>
             Hi, I&apos;m a web and app developer based in Bengaluru, India. I
@@ -33,6 +33,74 @@ export default function Home() {
           </a>
         </div>
       </header>
+      <div className={styles.sectionContainer}>
+        <section className={styles.textSection}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
+          </h1>
+          <p className={styles.subTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            delectus? Vero id atque rem est quis. Architecto adipisci id, iste
+            recusandae obcaecati porro doloremque? Accusantium at blanditiis
+            quis molestias doloribus.
+          </p>
+          <p className={styles.subTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            delectus? Vero id atque rem est quis. Architecto adipisci id, iste
+            recusandae obcaecati porro doloremque? Accusantium at blanditiis
+            quis molestias doloribus.
+          </p>
+          <p className={styles.subTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            delectus? Vero id atque rem est quis. Architecto adipisci id, iste
+            recusandae obcaecati porro doloremque? Accusantium at blanditiis
+            quis molestias doloribus.
+          </p>
+          <p className={styles.subTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            delectus? Vero id atque rem est quis. Architecto adipisci id, iste
+            recusandae obcaecati porro doloremque? Accusantium at blanditiis
+            quis molestias doloribus.
+          </p>
+          <p className={styles.subTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            delectus? Vero id atque rem est quis. Architecto adipisci id, iste
+            recusandae obcaecati porro doloremque? Accusantium at blanditiis
+            quis molestias doloribus.
+          </p>
+        </section>
+        <section className={styles.imageSection}>
+          <div className={styles.aboutImage}></div>
+          <div className={styles.contactContainer}>
+            <ul>
+              <li>
+                <span>
+                  <FaTwitter className={styles.icon} />
+                </span>
+                <span>Follow on Twitter</span>
+              </li>
+              <li>
+                <span>
+                  <FaInstagram className={styles.icon} />
+                </span>
+                <span>Follow on Instagram</span>
+              </li>
+              <li>
+                <span>
+                  <FaGithub className={styles.icon} />
+                </span>
+                <span>Follow on GitHub</span>
+              </li>
+              <li>
+                <span>
+                  <FaLinkedin className={styles.icon} />
+                </span>
+                <span>Follow on LinkedIn</span>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,21 +1,27 @@
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
+import { FaHome } from "react-icons/fa";
 
 export default function Header() {
   return (
     <nav className={styles.navbar}>
       <ul>
         <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/articles">Articles</Link>
+          <Link href="/">
+            <span>
+              <FaHome className={styles.icon} />
+            </span>
+            Home
+          </Link>
         </li>
         <li>
           <Link href="/projects">Projects</Link>
         </li>
         <li>
           <Link href="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link href="/articles">Articles</Link>
         </li>
         <li>
           <Link href="/contact">Contact</Link>
