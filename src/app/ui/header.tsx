f5c948bf3459d696b57f62a3dd6400e16b9196dc
Abhinav Sorate <abhinav.sorate@gida.io>
2023-11-26 19:55:55 +0530

    create menu bar for mobile devices

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,32 +1,79 @@
+"use client";
+
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaAngleDown } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
+
+  function showMenu(): void {
+    setToggle((toggle) => !toggle);
+  }
+
   return (
-    <nav className={styles.navbar}>
-      <ul>
-        <li>
-          <Link href="/">
-            <span>
-              <FaHome className={styles.icon} />
-            </span>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/skills">Skills</Link>
-        </li>
-        <li>
-          <Link href="/articles">Articles</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <Link href="/">
+              <span>
+                <FaHome className={styles.icon} />
+              </span>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link href="/articles">Articles</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.menubar}>
+        <div onClick={() => showMenu()} className={styles.dropdownButton}>
+          <span>Menu</span>
+          <span>
+            <FaAngleDown />
+          </span>
+        </div>
+        {toggle && (
+          <div className={styles.dropdownOptions}>
+            <div className={styles.dropdownHeader}>
+              <span>Navigation</span>
+              <button onClick={() => setToggle(false)}>
+                <IoClose />
+              </button>
+            </div>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link href="/skills">Skills</Link>
+              </li>
+              <li>
+                <Link href="/articles">Articles</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
