6aa4fe50a719cf880acacefd726dbf6aae6b980d
Abhinav Sorate <abhinav.sorate@gida.io>
2023-11-28 17:14:36 +0530

    minor change

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -1,6 +1,7 @@
 import styles from "../../../styles/contact.module.scss";
 import { FiMail } from "react-icons/fi";
 import { BsPhone } from "react-icons/bs";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -23,20 +24,22 @@ export default function Page() {
         <section className={styles.contactInfo}>
           <div className={styles.mail}>
             <div className={styles.mailIcon}>
-              <FiMail size={40}/>
+              <FiMail size={40} />
             </div>
             <div className={styles.mailText}>
               <div>Mail me at</div>
-              <div>abhinavsorate01@gmail.com</div>
+              <Link href={"mailto: abhinavsorate01@gmail.com"}>
+                abhinavsorate01@gmail.com
+              </Link>
             </div>
           </div>
           <div className={styles.phone}>
             <div className={styles.phoneIcon}>
-              <BsPhone size={40}/>
+              <BsPhone size={40} />
             </div>
             <div className={styles.phoneText}>
               <div>Call me at</div>
-              <div>955-277-3945</div>
+              <Link href={"tel: 955-277-3945"}>955-277-3945</Link>
             </div>
           </div>
         </section>
