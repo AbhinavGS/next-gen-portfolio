3c7cba5b34ceb8ef8cce284b12a83ba301ee0c9e
Abhinav Sorate <abhinav.sorate@gida.io>
2023-12-20 19:48:08 +0530

    made project cards clickable

--- src/app/ui/projectCard.tsx
+++ src/app/ui/projectCard.tsx
@@ -1,23 +1,26 @@
+import Link from "next/link";
 import styles from "../../styles/projects.module.scss";
 import Image from "next/image";
 
-export default function ProjectCard({ data }:any) {
+export default function ProjectCard({ data }: any) {
   return (
     <div className={styles.card}>
-      <Image
-        src={data.image}
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: "100%", height: "auto" }}
-        alt=""
-      ></Image>
-      <div className={styles.cardInfo}>
-        <p className={styles.title}>{data.title}</p>
-        <p className={styles.description}>{data.description}</p>
-        {/* <div>{data.repositoryLink}</div>
+      <Link href={data.link} target="_blank">
+        <Image
+          src={data.image}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+          alt=""
+        ></Image>
+        <div className={styles.cardInfo}>
+          <p className={styles.title}>{data.title}</p>
+          <p className={styles.description}>{data.description}</p>
+          {/* <div>{data.repositoryLink}</div>
       <div>{data.link}</div> */}
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
