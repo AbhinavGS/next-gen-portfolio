fbd3676aef589d59c7eaf0868698fced17ec5a7e
Abhinav Sorate <abhinav.sorate@gida.io>
2023-12-23 20:30:01 +0530

    add links to project cards

--- src/app/ui/projectCard.tsx
+++ src/app/ui/projectCard.tsx
@@ -1,26 +1,37 @@
 import Link from "next/link";
 import styles from "../../styles/projects.module.scss";
 import Image from "next/image";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 export default function ProjectCard({ data }: any) {
   return (
     <div className={styles.card}>
-      <Link href={data.link} target="_blank">
-        <Image
-          src={data.image}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          alt=""
-        ></Image>
-        <div className={styles.cardInfo}>
-          <p className={styles.title}>{data.title}</p>
-          <p className={styles.description}>{data.description}</p>
-          {/* <div>{data.repositoryLink}</div>
-      <div>{data.link}</div> */}
+      <Image
+        src={data.image}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+        alt=""
+      ></Image>
+      <div className={styles.cardInfo}>
+        <p className={styles.title}>{data.title}</p>
+        <div className={styles.linkWrapper}>
+          <span className={styles.links}>
+            <a href={data.link}>
+              <FaLink size={15} />
+              <span>View Demo</span>
+            </a>
+          </span>
+          <span className={styles.links}>
+            <a href={data.repositoryLink}>
+              <FaGithub size={15} />
+              <span>View Code</span>
+            </a>
+          </span>
         </div>
-      </Link>
+        <p className={styles.description}>{data.description}</p>
+      </div>
     </div>
   );
 }
