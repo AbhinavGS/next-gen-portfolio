7a190ea201b0da0177f7e662e211ec1900857c67
Abhinav Sorate <abhinav.sorate@gida.io>
2023-12-24 11:32:15 +0530

    add validations and loader on contact form

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -4,25 +4,60 @@ import styles from "../../../styles/contact.module.scss";
 import { FiMail } from "react-icons/fi";
 import { BsPhone } from "react-icons/bs";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 export default function Page() {
-  const form = useRef(null);
+  const form = useRef<HTMLFormElement>(null);
+  const nameInput = useRef<HTMLInputElement>(null);
+  const emailInput = useRef<HTMLInputElement>(null);
+  const messageInput = useRef<HTMLTextAreaElement>(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const areAllFormFieldsFilled = () => {
+    if (
+      nameInput.current?.value !== "" &&
+      emailInput.current?.value !== "" &&
+      messageInput.current?.value !== ""
+    )
+      return true;
+
+    return false;
+  };
+
+  const isEmailValid = () => {
+    if (
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+        emailInput.current?.value as string
+      )
+    )
+      return true;
+
+    return false;
+  };
 
   const sendEmail = async (e: any) => {
     e.preventDefault();
-
-    try {
-      const result = await emailjs.sendForm(
-        process.env.NEXT_PUBLIC_SANITY_MAILJS_SERVICE_ID ?? "",
-        process.env.NEXT_PUBLIC_SANITY_MAILJS_TEMPLATE_ID ?? "",
-        form.current ?? "",
-        process.env.NEXT_PUBLIC_SANITY_MAILJS_PUBLIC_KEY
-      );
-      console.log(result.text);
-    } catch (error: any) {
-      console.error(error.text);
+    if (areAllFormFieldsFilled() && isEmailValid()) {
+      setLoading(true);
+      try {
+        const result = await emailjs.sendForm(
+          process.env.NEXT_PUBLIC_SANITY_MAILJS_SERVICE_ID ?? "",
+          process.env.NEXT_PUBLIC_SANITY_MAILJS_TEMPLATE_ID ?? "",
+          form.current ?? "",
+          process.env.NEXT_PUBLIC_SANITY_MAILJS_PUBLIC_KEY
+        );
+        console.log(result.text);
+        form.current!.reset();
+        setLoading(false);
+      } catch (error: any) {
+        console.error(error.text);
+        form.current!.reset();
+        setLoading(false);
+      }
+    } else {
+      console.log("Please fill all the fields appropriately");
     }
   };
 
@@ -34,15 +69,30 @@ export default function Page() {
         </section>
         <div className={styles.contactSection}>
           <form ref={form} className={styles.contactForm}>
-            <input type="text" placeholder="Enter your name" name="name" />
-            <input type="text" placeholder="Enter your email" name="email" />
+            <input
+              ref={nameInput}
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+            />
+            <input
+              ref={emailInput}
+              type="text"
+              placeholder="Enter your email"
+              name="email"
+            />
             <textarea
+              ref={messageInput}
               placeholder="Enter your message"
               name="message"
               cols={30}
               rows={10}
             ></textarea>
-            <button onClick={(e) => sendEmail(e)}>Send message</button>
+            {loading ? (
+              <span className={styles.loader}></span>
+            ) : (
+              <button onClick={(e) => sendEmail(e)}>Send message</button>
+            )}
           </form>
           <section className={styles.contactInfo}>
             <div className={styles.mail}>
